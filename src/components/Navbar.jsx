624488c76940ae87edef531fcef78886b90a7803
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-01 09:30:31 +0100

    added logo to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
@@ -7,11 +7,21 @@ import { logo, menu, close } from "../assets";
 
 
 const Navbar = () => {
-return (
-  <div>
-    Hello
-  </div>
-);
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
+          <img src={logo} alt="" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Prawal &nbsp;  <span className="sm:block hidden">Parajuli</span></p>
+        </Link>
+      
+      
+      </div>
+    </nav>
+  );
 };
 
 
