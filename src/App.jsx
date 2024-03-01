93634a79e15bba5eb5c20b8391cdb3f1f38a9ed5
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-01 09:43:03 +0100

    added background to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
       <div className="relative z-0 bg-primary">
-       <div className="">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar />
-           
+            <Hero />
           </div>
        </div>
       </BrowserRouter>
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <section className="relative w-full h-screen mx-auto">
+        
+      </section>
+    );
+}
+
+export default Hero;
