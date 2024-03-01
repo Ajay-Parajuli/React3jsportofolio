57d82adc6b359d93e1f1d49249feaa896a66516f
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-01 09:57:08 +0100

    setting up out 3d object inside mesh using useGLTF

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,6 +3,32 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
+
+
+const Computers = ({ isMobile }) => {
+    const { scene } = useGLTF("./desktop_pc/scene.gltf"); 
+    return (
+      <mesh> 
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <pointLight intensity={1} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize-width={1024}
+         />
+        <primitive object={scene} 
+        scale={isMobile ? 0.7 : 0.75} 
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]} />
+      </mesh>
+    );
+  };
+
+
+
 const ComputersCanvas = () => {
 
 };
