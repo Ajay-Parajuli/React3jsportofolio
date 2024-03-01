14426cc46762709ed8066fbc4fcc80da3e2fbe63
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-01 09:33:17 +0100

    rendered navlinks dynamically

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,8 +17,13 @@ const Navbar = () => {
           <img src={logo} alt="" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">Prawal &nbsp;  <span className="sm:block hidden">Parajuli</span></p>
         </Link>
-      
-      
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => ( 
+            <li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] cursor-pointer`} onClick={() => setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
