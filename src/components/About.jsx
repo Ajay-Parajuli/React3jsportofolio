90f318f55dca4872ca0977353f067179f8a06cc8
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-02 07:19:27 +0100

    imported neccessary imports

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc"; 
+
+
+const About = () => {
+
+};
+
+export default About;
