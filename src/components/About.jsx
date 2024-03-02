3fe4e36ba162553ca2e081e4e5d65a1ac1e8dc0c
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-02 08:14:52 +0100

    created intro text for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,26 @@
-import { Tilt } from "react-tilt";
+
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { services } from "../constants";
+
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc"; 
 
 
 const About = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}
+      >Introduction</p>
+      <h2 className={styles.sectionHeadText}
+      >Overview</h2>
+    </motion.div>
+    <motion.p variants={fadeIn("", "", 0.1, 0.1)}
+    className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+I specialize as a Fullstack Developer. My skill set spans a diverse range of technologies, including Remix, Node.js, Express, PHP, MongoDB, and MySQL. I am always learning and trying new things to make sure I can build the best websites and apps possible </motion.p>
+    </>
+  )
+}
 
-};
-
-export default About;
+export default SectionWrapper(About, "about")
