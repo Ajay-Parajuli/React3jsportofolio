cfc4283c244d33d81c29b04b22ae5a07fc5a7ec7
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-02 12:56:32 +0100

    created a 3d ball object with floating animation, lighting, shading. It is ready to be rendered using canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -4,16 +4,33 @@ import { OrbitControls, Preload, useTexture, Decal, Float } from "@react-three/d
 import CanvasLoader from "../Loader";
 
 
-const Ball = (props) => {
- 
-}
+const Ball = (props) => { // Change the function signature to accept props
+    const [decal] = useTexture([props.imgUrl]); // Use props.imgUrl directly
+    
+    return (
+      <Float speed={1.75} rotationIntensity={1} floatIntensity={2} >
+        <ambientLight intensity={0.25}/>
+        <directionalLight position={[0, 0, 0.05]} />
+        <mesh castShadow receiveShadow scale={2.75}>
+          <icosahedronGeometry args={[1, 1]} />
+          <meshStandardMaterial color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading />
+          <Decal position={[0, 0, 1]}
+          rotation={[2*Math.PI, 0, 6.25]} map={decal}
+          flatShading />
+        </mesh>
+      </Float>
+    );
+  };
 
 const BallCanvas = ({ icon }) => {
-  
-  
     return (
      <></>
     );
   };
+  
+
   
   export default BallCanvas;
