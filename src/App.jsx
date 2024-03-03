6e7e86a87b9c100e1b1dce4a0a3b0f74c4126150
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-03 09:49:20 +0100

    imported neccessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Tech from "./components/Tech";
 import Education from "./components/Education";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <About />
           <Tech />
           <Education />
+          <Projects />
        </div>
       </BrowserRouter>
     </>
