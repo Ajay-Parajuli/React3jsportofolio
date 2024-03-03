cea365407a7bec1da4a9f541efd6a1a5334071fd
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-03 10:30:47 +0100

    added picture to the project chatapp

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const ProjectCard = ({name, description, index, source_code_link, image, tags, }
           speed: 450,
         }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
           <div className="relative h-[230px] w-full"> 
-            <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
+            <img src={image} alt={name} className="w-full h-full object-contain " />
             <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
               <div onClick={() => window.open
               (source_code_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
@@ -50,9 +50,31 @@ const ProjectCard = ({name, description, index, source_code_link, image, tags, }
   }
 
 
-  
-const Projects = () => {
 
+const Projects = () => {
+  return (
+    <>
+    <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Projects</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p variants={fadeIn("", "", 0.1, 1)}
+        className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          Here are some of the projects I have worked on. You can find more of my work on my github profile.
+        </motion.p>
+      </div>
+      <div className='flex flex-wrap mt-20 gap-7 '>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`}
+          index={index}
+          {...project} />
+       ))}
+      </div>
+    </>
+  )
 };
 
 export default SectionWrapper(Projects, '')
--- src/components/index.js
+++ src/components/index.js
@@ -3,20 +3,16 @@ import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+import Education from './Education';
+import Projects from './Projects';
 
 export {
   Hero,
   Navbar,
   About,
   Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
+  Education,
+  Projects,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
