0751ad42890fb258203f87972e8d8a84710db695
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2024-03-03 16:56:57 +0100

    tailwind adjustments using tailwind sorter

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,27 +16,27 @@ const ProjectCard = ({name, description, index, source_code_link, image, tags, }
           max: 45,
           scale: 1,
           speed: 450,
-        }} className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-          <div className="relative h-[230px] w-full"> 
-            <img src={image} alt={name} className="w-full h-full object-contain " />
+        }} className="bg-tertiary p-5 rounded-2xl w-full sm:w-[360px]">
+          <div className="relative w-full h-[230px]"> 
+            <img src={image} alt={name} className="w-full h-full object-contain" />
             <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
               <div onClick={() => window.open
-              (source_code_link, "_blank")} className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
+              (source_code_link, "_blank")} className="flex justify-center items-center rounded-full w-10 h-10 cursor-pointer black-gradient">
                 <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
               </div>
             </div>
           </div>
   
           <div className="mt-5">
-                <h3 className="text-white font-bold text-[24px] ">
+                <h3 className="font-bold text-[24px] text-white">
                   {name}
                 </h3>
-                <p className="text-secondary mt-3 text-[14px]">
+                <p className="mt-3 text-[14px] text-secondary">
                   {description}
                   </p>
           </div>
   
-          <div className="mt-4 flex flex-wrap gap-2">
+          <div className="flex flex-wrap gap-2 mt-4">
               {tags.map((tag) => (
                 <p key={tag.name} className={`text-[14px] ${tag.color}`}>
                   #{tag.name}
@@ -59,14 +59,14 @@ const Projects = () => {
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='flex w-full'>
         <motion.p variants={fadeIn("", "", 0.1, 1)}
-        className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className='mt-3 max-w-3xl text-[17px] text-secondary leading-[30px]'
         >
           Here are some of the projects I have worked on. You can find more of my work on my github profile.
         </motion.p>
       </div>
-      <div className='flex flex-wrap mt-20 gap-7 '>
+      <div className='flex flex-wrap gap-7 mt-20'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`}
           index={index}
